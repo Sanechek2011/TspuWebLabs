49c7d94fa40d72ba6a57df19ed8654f9298a4dca
USER-PC\Alex <sanechek2011@bk.ru>
2023-06-05 17:35:15 +0300

    no message

--- Contracts/AddUserContract.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TspuWebLabs.Contracts
-{
-    public class AddUserContract
-    {
-        public string Name { get; set; }
-        public string Login { get; set; }
-        public string Password { get; set; }
-    }
-}
